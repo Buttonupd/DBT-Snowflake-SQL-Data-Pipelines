7c174e6c05bb310aa331c8f4c21fbcff07593a74
Daniel Kariuki <kariuki.daniel@eclectics.io>
2023-08-30 21:55:45 +0300

    Enviroment variable obfuscation

--- dags/retail.py
+++ dags/retail.py
@@ -8,6 +8,7 @@ from airflow.providers.google.cloud.transfers.mssql_to_gcs import MSSQLToGCSOper
 default_args = {
     "start_date": datetime(2023, 8, 30)
 }
+
 @dag(
     schedule=None,
     default_args=default_args,
@@ -16,39 +17,38 @@ default_args = {
     )
 
 def retail():
-    f = MSSQLToGCSOperator(
-    task_id = 'f',
+    upload_sql_stat = MSSQLToGCSOperator(
+    task_id = 'upload_sql_stat',
     sql=config('sql'),
     bucket=config("bucket"),
     filename="raw/imdb.csv",
     gcp_conn_id="conn_id",
     mssql_conn_id="sql_connid",
     export_format="csv"
-
 )
-
 retail()
 
-def my_dag():
+def connector():
 
-    @task(task_id='testsql')
-    def test_sql():
+    @task(task_id='sql_conn')
+    def sql_conn():
         conn = pymssql.connect(
             server=config('server'),
             user=config('user'),
             password=config('password'),
-            database=config('database')
-            
-    )
-        query1 = """
-            select Top 10 * from transactions.imdb
-            """
+            database=config('database')    
+            )
+        
+        schema = config("schema")
+        query1 = f"""
+            select Top 10 * from {schema}
+                 """
         cursor = conn.cursor()
         queryresult  = cursor.execute(query1)
         records = cursor.fetchall()
         for r in records:
             pass
     
-    test_sql()
+    sql_conn()
 
-my_dag()
+connector()
