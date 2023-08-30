c2ab651cf4c8773ed071ff721922fa5ec4b05598
Daniel Kariuki <kariuki.daniel@eclectics.io>
2023-08-30 21:47:35 +0300

    Business logic Ingest imdb data from a local instance of mssql using the MSSQLToGCSOperator module into a gcs bucket.

--- dags/retail.py
+++ dags/retail.py
@@ -1,33 +1,38 @@
-import json
-from airflow.models.connection import Connection
-import pandas as pd
-from pathlib import Path
 from airflow import DAG
-from airflow.operators.python import PythonOperator,BranchPythonOperator
 from airflow.decorators import task,dag
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
-import time
 from datetime import datetime, timedelta
 import pymssql
 from decouple import config
+from airflow.providers.google.cloud.transfers.mssql_to_gcs import MSSQLToGCSOperator
 
 default_args = {
     "start_date": datetime(2023, 8, 30)
 }
-
-
 @dag(
-    schedule="@daily",
+    schedule=None,
     default_args=default_args,
+    tags = 'retail',
     catchup=False, dagrun_timeout=timedelta(minutes=10), max_active_tasks=2, max_active_runs=1
     )
 
+def retail():
+    f = MSSQLToGCSOperator(
+    task_id = 'f',
+    sql=config('sql'),
+    bucket=config("bucket"),
+    filename="raw/imdb.csv",
+    gcp_conn_id="conn_id",
+    mssql_conn_id="sql_connid",
+    export_format="csv"
+
+)
+
+retail()
+
 def my_dag():
+
     @task(task_id='testsql')
     def test_sql():
-    
-
         conn = pymssql.connect(
             server=config('server'),
             user=config('user'),
@@ -35,15 +40,15 @@ def my_dag():
             database=config('database')
             
     )
-        
-     
-        query1 = "select Top 100 * from TestData"
-        print(query1)
-        print(conn)
+        query1 = """
+            select Top 10 * from transactions.imdb
+            """
         cursor = conn.cursor()
         queryresult  = cursor.execute(query1)
-
-        print(queryresult)
+        records = cursor.fetchall()
+        for r in records:
+            pass
     
     test_sql()
+
 my_dag()
