fe3ba05536d32f66e9f2a96bdcb4a3528d68277f
Daniel Kariuki <kariuki.daniel@eclectics.io>
2023-08-31 12:00:09 +0300

    Create a DAG which uses python astro-sdk to read from a gcs bucket and ingest the file into bigquery dataset

--- dags/retail.py
+++ dags/retail.py
@@ -4,17 +4,20 @@ from datetime import datetime, timedelta
 import pymssql
 from decouple import config
 from airflow.providers.google.cloud.transfers.mssql_to_gcs import MSSQLToGCSOperator
-
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table,Metadata
+from astro.constants import FileType
 default_args = {
     "start_date": datetime(2023, 8, 30)
 }
-
 @dag(
-    schedule=None,
+    schedule="@daily",
     default_args=default_args,
-    tags = 'retail',
     catchup=False, dagrun_timeout=timedelta(minutes=10), max_active_tasks=2, max_active_runs=1
     )
+
 
 def retail():
     upload_sql_stat = MSSQLToGCSOperator(
@@ -25,30 +28,51 @@ def retail():
     gcp_conn_id="conn_id",
     mssql_conn_id="sql_connid",
     export_format="csv"
-)
-retail()
+    )
 
-def connector():
+    create_imdb_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_imdb_dataset',
+        dataset_id='imdb',
+        gcp_conn_id='conn_id'
 
-    @task(task_id='sql_conn')
-    def sql_conn():
+    )
+    upload_idb_dataset = aql.load_file(
+        task_id='upload_idb_dataset',
+        input_file=File(
+            'gs://daniel_online_retail/raw/imdb.csv',
+            conn_id='conn_id',
+            filetype=FileType.CSV
+        ),
+        output_table=Table(
+            name='raw_imdb',
+            conn_id='conn_id',
+            metadata=Metadata(schema='imdb')
+        ),
+        use_native_support=False
+    )
+
+    @task(task_id='sqlconn')
+    def sql_Conn():
         conn = pymssql.connect(
             server=config('server'),
             user=config('user'),
             password=config('password'),
-            database=config('database')    
-            )
-        
-        schema = config("schema")
-        query1 = f"""
-            select Top 10 * from {schema}
-                 """
+            database=config('database')
+            
+    )
+        schema = config('schema')
+        queryStatement = f"""
+                        select Top 10 * from {schema}
+                        """
         cursor = conn.cursor()
-        queryresult  = cursor.execute(query1)
+        records  = cursor.execute(queryStatement)
         records = cursor.fetchall()
         for r in records:
-            pass
+            print(r)
     
-    sql_conn()
+    sql_Conn()
 
-connector()
+retail()
+
+
+
