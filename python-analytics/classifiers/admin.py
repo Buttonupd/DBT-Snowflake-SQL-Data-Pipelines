1b8fbfbd0fde4366993ac816cfd5d1b49e8f87c5
Daniel Kariuki <kariuki.daniel@eclectics.io>
2023-09-06 11:52:10 +0300

    Create a movie and sentiment schema

--- python-analytics/classifiers/admin.py
+++ python-analytics/classifiers/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Movie, Sentiment
 
-# Register your models here.
+class SentimentAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        model = Sentiment
+        
+        list_display = [
+            'id',
+            'movie',
+            'review',
+            'results',
+            'gender',
+            'age',
+            'country',
+            'timestamp',
+            'updated'
+        ]
+admin.site.register(Sentiment, SentimentAdmin)
+
+class MovieAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Movie
+        list_display = [
+            'id',
+            'title'
+        ]
+admin.site.register(Movie, MovieAdmin)
+
+
